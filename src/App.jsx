c33eeb579d89fba3696187816767145225505c20
Aher-Nikhil <nikhilaher125@gmail.com>
2025-03-28 10:30:52 +0530

    add Above page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ApplicationForm from './Components/ApplicationForm'
 import OnlineTest from './Components/OnlineTest'
-
+import About from './Components/About'
 const App = () => {
   return (
     <>
       <ApplicationForm/>
       <OnlineTest/>
+      <About/>
     </>
 
   )
